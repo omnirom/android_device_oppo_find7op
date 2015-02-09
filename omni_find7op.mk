60ad5c46ce3794d35ef0fc5a5795c79bf37d6683
Menno van Grinsven <scanno71@gmail.com>
2015-02-09 12:09:23 +0100

    find7op: [WIP] Use new blobs
    Change-Id: I5d5703b028b4db164e64ab3f1d1a9363ceebc8cd

--- omni_find7op.mk
+++ omni_find7op.mk
@@ -56,5 +56,5 @@ TARGET_KERNEL_CONFIG := msm8974_find7op_defconfig
 
 # Inherit from proprietary blobs
 $(call inherit-product-if-exists, vendor/oppo/find7op/find7op-vendor.mk)
-$(call inherit-product-if-exists, vendor/oppo/find7a/find7a-vendor.mk)
+$(call inherit-product-if-exists, vendor/oppo/msm8974-common/msm8974-common-vendor.mk)
 
