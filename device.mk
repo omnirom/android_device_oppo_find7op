57c0ab01b996eb7236e61d1197cde26faf85e30a
Menno van Grinsven <scanno71@gmail.com>
2015-07-14 21:38:08 +0200

    find7op: Add NFC HCE permission file to device tree
    Change-Id: I28e88feb0a4b2ebef1775704d091509e7f3cd2b4

--- device.mk
+++ device.mk
@@ -23,6 +23,7 @@ PRODUCT_PACKAGES += \
     Tag
 
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
     $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/etc/libnfc-brcm.conf
 
