e040d91f06c85930af2c9f2f2cf1d5ea168625bd
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2015-11-02 18:38:45 +0100

    TEST: find7op changes
    Change-Id: Ice6778a5a2fc39d5757953acbc68d5810289d2ae

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 # Include common makefile
 $(call inherit-product, device/oppo/msm8974-common/common.mk)
 
+$(call inherit-product, frameworks/native-caf/build/phone-xxxhdpi-3072-dalvik-heap.mk)
+$(call inherit-product, frameworks/native-caf/build/phone-xxxhdpi-3072-hwui-memory.mk)
+
 LOCAL_PATH := device/oppo/find7op
 
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
@@ -30,6 +33,10 @@ PRODUCT_COPY_FILES += \
 # Sensor configuration from Oppo
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/sensor/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf
+
+# Snapdragon Camera
+PRODUCT_PACKAGES += \
+    SnapdragonCamera
 
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
