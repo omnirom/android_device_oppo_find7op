1cfac1b43ff42c8d6d76dc0f8f166e5b39d97678
Hernán Castañón Álvarez <hernanderes34@gmail.com>
2015-11-02 18:38:45 +0100

    TEST: find7op changes
    Change-Id: Ice6778a5a2fc39d5757953acbc68d5810289d2ae

--- omni_find7op.mk
+++ omni_find7op.mk
@@ -55,5 +55,5 @@ TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
 TARGET_KERNEL_CONFIG := msm8974_find7op_defconfig
 
 # Inherit from proprietary blobs
-$(call inherit-product, vendor/oppo/find7a/find7a-vendor.mk)
+$(call inherit-product, vendor/oppo/find7op/find7op-vendor.mk)
 
