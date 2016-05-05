fa984c33e972e5ce781d30ccf9ef2aea240101e3
Max Weninger <max.weninger@gmail.com>
2016-05-05 17:12:19 +0100

    Revert "find7op: config proxi sensor for new hal"
    This reverts commit 66449918b3b4a21c563872dcd8575a8c34b05a8f.
    
    Change-Id: I9e64a1ea95ef09f7a58b8371fa252631eef2937a

--- device.mk
+++ device.mk
@@ -35,6 +35,5 @@ PRODUCT_COPY_FILES += \
 # Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480 \
-    persist.camera.4k2k.enable=1 \
-    ro.qti.sensors.ir_proximity=true
+    persist.camera.4k2k.enable=1
 
