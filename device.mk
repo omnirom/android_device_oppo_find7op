90f225852f9ddb1dd13cbf8ebf3b7adce0f98838
Philipp Vogel <philippwolf86.pw@gmail.com>
2016-05-09 22:41:23 +0200

    find7op: prepare for snapdragoncamera
    Change-Id: If716901aa2c2b5f9588678a7a2a17dcb00d3482a

--- device.mk
+++ device.mk
@@ -22,7 +22,8 @@ PRODUCT_COPY_FILES += \
 # NFC packages
 PRODUCT_PACKAGES += \
     NfcNci \
-    nfc_nci.find7op
+    nfc_nci.find7op \
+		SnapdragonCamera
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
@@ -37,4 +38,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480 \
     persist.camera.4k2k.enable=1 \
     ro.qti.sensors.ir_proximity=true
-
